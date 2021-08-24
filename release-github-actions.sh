6342fb490d48f5acf2f1d8ba9b7c151224014cda
Ricardo Pinto <pinto.ricky@gmail.com>
2021-08-24 11:46:23 +0100

    Remocing chmod for scripts

--- release-github-actions.sh
+++ release-github-actions.sh
@@ -3,8 +3,6 @@ set -e
 export CI_COMMIT_SHA="${GITHUB_SHA}"
 export CI_COMMIT_REF_NAME="${GITHUB_REF}"
 
-chmod +x /usr/local/bin/release.sh
-chmod +x /usr/local/bin/gpg_config.sh
 echo "GPG Version"
 gpg --version
 release.sh
