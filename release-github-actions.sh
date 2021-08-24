fbb604eb7f2c6ba3ab6beafed0947dfc7a445468
Ricardo Pinto <pinto.ricky@gmail.com>
2021-08-24 11:41:06 +0100

    Echo gpg version

--- release-github-actions.sh
+++ release-github-actions.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
-
+set -e
 export CI_COMMIT_SHA="${GITHUB_SHA}"
 export CI_COMMIT_REF_NAME="${GITHUB_REF}"
 
 chmod +x /usr/local/bin/release.sh
 chmod +x /usr/local/bin/gpg_config.sh
+echo "GPG Version"
+gpg --version
 release.sh
